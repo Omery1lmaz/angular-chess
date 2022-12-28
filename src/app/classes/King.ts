ed85f7804d774eb9d2a1ecbf573949e5dcb946aa
Omer Yılmaz <omery020040@gmail.com>
2022-12-28 16:36:30 +0300

    update

--- src/app/classes/King.ts
+++ src/app/classes/King.ts
@@ -13,18 +13,22 @@ export class King extends Piece {
   }
   override canMove(move: Move) {
     const { board, toX, toY, piece, x, y, toBoard } = this.getParams(move)
-
+    const deneme = ((toX == this.x + 1) || (toX == this.x - 1))
+    const deneme1 = ((toY == this.y + 1) || (toY == this.y - 1))
+    const deneme2 = (toX == this.x) && ((toY == this.y + 1) || (toY == this.y - 1))
+    const deneme3 = ((toY == this.y) && ((toX == this.x - 1) || (toX == this.x + 1)))
     /*
     - X yönünde hareket konrolü
     - y yönünde hareket kontrolü
     - çapraz y yönünde hareket kontolü
     - çapraz x yönünde hareket kontrolü
     */
-    if ((((toX == this.x + 1) || (toX == this.x - 1))) && ((toY == this.y + 1) || (toY == this.y - 1)) || ((toX == this.x) && ((toY == this.y + 1) || (toY == this.y - 1)) || ((toY == this.y) && ((toX == this.x - 1) || (toX == this.x + 1))))) {
+    if (deneme && deneme1 || (deneme2 || deneme3)) {
       if (this.white == toBoard?.white) {
         console.log("Karşı taraf farklı ");
         return false;
       } else return true;
+      // return true
     } else {
       return false;
     }
